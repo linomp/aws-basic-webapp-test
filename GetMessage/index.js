01da21fb00a31fc17008c5eedca88dc8b162ea1b
linomp <lm2p@hotmail.es>
2021-10-24 13:11:41 +0200

    download Post/Get Message Lambda functions for doc purposes

--- GetMessage/index.js
+++ GetMessage/index.js
@@ -9,23 +9,29 @@ let now = date.toISOString();
 // Define handler function, the entry point to our code for the Lambda service
 // We receive the object that triggers the function as a parameter
 exports.handler = async (event) => {
-    // Extract values from event and format as strings
-    let name = `${event.firstName} ${event.lastName}`;
+   
     // Create JSON object with parameters for DynamoDB and store in a variable
     let params = {
         TableName:'HelloWorldDatabase',
-        Item: {
-            'ID': name,
-            'LatestGreetingTime': now
-        }
+         Key: {
+            ID: '1'
+          }
     };
     // Using await, make sure object writes to DynamoDB table before continuing execution
-    await dynamodb.put(params).promise();
-    // Create a JSON object with our response and store it in a constant
-    const response = {
-        statusCode: 200,
-        body: JSON.stringify(`Hello from browser-based Lambda, ${event.firstName} ${event.lastName}`)
-    };
-    // Return the response constant
-    return response;
+    let body = await dynamodb.get(params).promise();
+    
+    if(body && body.Item != undefined){ 
+        const response = {
+            statusCode: 200,
+            body: body.Item.text
+        }; 
+        return response;
+        
+    }else{
+        
+        return{
+            statusCode: 500,
+            body: undefined
+        };
+    } 
 };
---
+++ PostMessageFunction/index.js
@@ -0,0 +1,44 @@
+// Include the AWS SDK module
+const AWS = require('aws-sdk'); 
+
+// Instantiate a DynamoDB document client with the SDK
+let dynamodb = new AWS.DynamoDB.DocumentClient();
+// Use built-in module to get current date & time
+let date = new Date();
+// Store date and time in human-readable format in a variable
+let now = date.toISOString();
+// Define handler function, the entry point to our code for the Lambda service
+// We receive the object that triggers the function as a parameter
+exports.handler = async (event) => {
+    // Extract values from event and format as strings
+    let text = `${event.message}`;
+    // Create JSON object with parameters for DynamoDB and store in a variable
+    let params = {
+        TableName:'HelloWorldDatabase',
+        Item: {
+            'ID': "1", // update the message to show
+            'text': text
+        }
+    };
+    // Using await, make sure object writes to DynamoDB table before continuing execution
+    await dynamodb.put(params).promise();
+    
+    let params2 = {
+        TableName:'HelloWorldDatabase',
+        Item: {
+            'ID': JSON.stringify((new Date()).getTime()), // backup
+            'text': text
+        }
+    };
+    // Using await, make sure object writes to DynamoDB table before continuing execution
+    await dynamodb.put(params2).promise();
+    
+    
+    // Create a JSON object with our response and store it in a constant
+    const response = {
+        statusCode: 200,
+        body: JSON.stringify(`received: ${event.message}`)
+    };
+    // Return the response constant
+    return response;
+};
